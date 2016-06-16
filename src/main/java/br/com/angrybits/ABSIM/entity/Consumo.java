f2b43843182d45a454866e464cb7c88c1a8598f6
78142024500 <78142024500@serpro-1565870>
2016-06-16 16:14:31 -0300

    Criada entidade Consumo.

---
+++ src/main/java/br/com/angrybits/ABSIM/entity/Consumo.java
@@ -0,0 +1,59 @@
+package br.com.angrybits.ABSIM.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Consumo implements Serializable{
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private int id;
+	
+	private String consumo;	
+
+	public int getId() {
+		return id;
+	}
+	
+	public String getConsumo() {
+		return consumo;
+	}
+
+	public void setConsumo(String consumo) {
+		this.consumo = consumo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Consumo other = (Consumo) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
