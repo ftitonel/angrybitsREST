5f0fca6e94b396621b3350769bf10a775fb8e57a
78142024500 <78142024500@serpro-1565870>
2016-06-16 16:58:41 -0300

    Criação BC e DAO.

---
+++ src/main/java/br/com/angrybits/ABSIM/business/ConsumoBC.java
@@ -0,0 +1,11 @@
+package br.com.angrybits.ABSIM.business;
+
+import br.com.angrybits.ABSIM.entity.Consumo;
+import br.com.angrybits.ABSIM.persistence.ConsumoDAO;
+import br.gov.frameworkdemoiselle.stereotype.BusinessController;
+import br.gov.frameworkdemoiselle.template.DelegateCrud;
+
+@BusinessController
+public class ConsumoBC extends DelegateCrud<Consumo, Long, ConsumoDAO>{
+
+}
---
+++ src/main/java/br/com/angrybits/ABSIM/persistence/ConsumoDAO.java
@@ -0,0 +1,15 @@
+package br.com.angrybits.ABSIM.persistence;
+
+import br.com.angrybits.ABSIM.entity.Consumo;
+import br.gov.frameworkdemoiselle.stereotype.PersistenceController;
+import br.gov.frameworkdemoiselle.template.JPACrud;
+
+@PersistenceController
+public class ConsumoDAO extends JPACrud<Consumo, Long>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
